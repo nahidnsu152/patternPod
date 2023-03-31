00f81574d2bfc83c9d2f8eff08f0ec36c1138391
Nazmul Hasan <nazmul.hasan152@northsouth.edu>
2023-03-31 23:57:28 +0600

    flutter version updated, new extention added

--- lib/utils/extentions.dart
+++ lib/utils/extentions.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+extension GlobalKeyExtension on GlobalKey {
+  Rect get globalPaintBounds {
+    final renderObject = currentContext?.findRenderObject();
+    final translation = renderObject?.getTransformTo(null).getTranslation();
+    if (translation != null && renderObject?.paintBounds != null) {
+      final offset = Offset(translation.x, translation.y);
+      return renderObject!.paintBounds.shift(offset);
+    } else {
+      return Rect.zero;
+    }
+  }
+}
+
 extension StringCasingExtension on String {
   String toCapitalize() =>
       length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
@@ -63,19 +76,6 @@ extension DateTimeExtension on DateTime {
   }
 }
 
-extension GlobalKeyExtension on GlobalKey {
-  Rect get globalPaintBounds {
-    final renderObject = currentContext?.findRenderObject();
-    final translation = renderObject?.getTransformTo(null).getTranslation();
-    if (translation != null && renderObject?.paintBounds != null) {
-      final offset = Offset(translation.x, translation.y);
-      return renderObject!.paintBounds.shift(offset);
-    } else {
-      return Rect.zero;
-    }
-  }
-}
-
 extension NumberConverter on num {
   static const Map<String, String> arabicDigits = <String, String>{
     '0': '\u0660',
@@ -97,5 +97,64 @@ extension NumberConverter on num {
       sb.write(arabicDigits[number[i]] ?? number[i]);
     }
     return sb.toString();
+  }
+}
+
+extension SeparatedList<T> on List<T> {
+  List<T> separated(T separator) {
+    final newList = <T>[];
+    for (var i = 0; i < length; i++) {
+      if (i == 0) {
+        newList.add(this[i]);
+      } else {
+        newList.add(separator);
+        newList.add(this[i]);
+      }
+    }
+    return newList;
+  }
+}
+
+extension ThemedSlider on Slider {
+  static Widget withTheme({
+    Key? key,
+    required double value,
+    required Function(double) onChanged,
+    Function(double)? onChangeStart,
+    Function(double)? onChangeEnd,
+    double min = 0.0,
+    double max = 1.0,
+    int? divisions,
+    String? label,
+    Color? activeColor,
+    Color? inactiveColor,
+    Color? thumbColor,
+    MouseCursor? mouseCursor,
+    String Function(double)? semanticFormatterCallback,
+    FocusNode? focusNode,
+    bool autofocus = false,
+    required SliderThemeData themeData,
+  }) {
+    return SliderTheme(
+      data: themeData,
+      child: Slider(
+        key: key,
+        value: value,
+        onChanged: onChanged,
+        onChangeStart: onChangeStart,
+        onChangeEnd: onChangeEnd,
+        min: min,
+        max: max,
+        divisions: divisions,
+        label: label,
+        activeColor: activeColor,
+        inactiveColor: inactiveColor,
+        thumbColor: thumbColor,
+        mouseCursor: mouseCursor,
+        semanticFormatterCallback: semanticFormatterCallback,
+        focusNode: focusNode,
+        autofocus: autofocus,
+      ),
+    );
   }
 }
