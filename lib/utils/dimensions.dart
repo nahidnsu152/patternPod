398d4f50a29da23c722396e8625a30dea696ff9a
nahidnsu152 <nahid.blackeyes@gmail.com>
2023-08-21 18:25:54 +0600

    Dimensions file updated

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -2,15 +2,15 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Dimensions {
   Dimensions._();
-
-  static double defaultTextSize = 16.00.sp;
-  static double mediumTextSize = 18.00.sp;
-  static double textSize14 = 14.00.sp;
+  static double smallTextSize = 12.00.sp;
+  static double defaultTextSize = 14.00.sp;
+  static double mediumTextSize = 16.00.sp;
+  static double textSize10 = 10.00.sp;
+  static double textSize18 = 18.00.sp;
   static double textSize20 = 20.00.sp;
   static double textSize22 = 22.00.sp;
   static double textSize24 = 24.00.sp;
   static double textSize26 = 26.00.sp;
+  static double textSize28 = 28.00.sp;
   static double bigTextSize = 30.00.sp;
-  static double smallTextSize = 12.00.sp;
-  static double textSize10 = 10.00.sp;
 }
