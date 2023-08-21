8a5dfd153875afe6ddc6cfc455cb4e4ac7a23f84
nahidnsu152 <nahid.blackeyes@gmail.com>
2023-08-21 16:51:56 +0600

    color extention added

--- lib/utils/extentions.dart
+++ lib/utils/extentions.dart
@@ -3,6 +3,30 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
+
+// extension HexColor on Color {
+//   static Color getColorFromHex(String hexColor) {
+//     hexColor = hexColor.toUpperCase().replaceAll("#", "");
+//     if (hexColor.length == 6) {
+//       hexColor = "FF$hexColor";
+//     }
+//     return Color(int.parse(hexColor, radix: 16));
+//   }
+
+//   static Color fromHex(String hexColor) => getColorFromHex(hexColor);
+// }
+
 String generateRandomString(int len) {
   var r = Random();
   const chars =
